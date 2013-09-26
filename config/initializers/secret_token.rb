41fb4d25e0872749e1f03cccd34faad7abbd6e2b
Joel Mayer <joelmayer@Andys-Mac-Pro.local>
2013-09-26 17:50:12 +0100

    Initial commit - Devise and first models

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Survey::Application.config.secret_token = '2cccb6e42197153ef8841505116d696fa9be22f62009e6a0ac6172be67f524789240bacecbfbcd59f63c5b499f7335e516e9b72364c9c0419fcaca8550912806'
