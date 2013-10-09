a08ac4b6efa4d67bfa5d0f8e121c731fe26611ee
Joel Mayer <joelmayer@Andys-Mac-Pro.local>
2013-10-09 18:32:44 +0100

    Added start for ajax btn toggles

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -3,4 +3,18 @@ $(document).ready(function() {
   return $('.section').hover(function(event) {
     return $(this).toggleClass("hover");
   });
+	
+	$('#yes').click(function(event) {
+	  $.ajax({
+			$('#yes').html("<a href='#' class='btn btn-success'>Yes</a>");
+			$('#no').html("<a href='#' class='btn'>No</a>");
+		});
+	});
+	
+	$('#no').click(function(event) {
+	  $.ajax({
+			$('#no').html("<a href='#' class='btn btn-danger'>No</a>");
+			$('#yes').html("<a href='#' class='btn'>Yes</a>");
+		});
+	});
 });
