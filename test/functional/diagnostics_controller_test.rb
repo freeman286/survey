a606485c91cc271f716cb5fc10990d274415c7b4
Joel Mayer <joelmayer@Andys-Mac-Pro.local>
2013-10-30 14:21:46 +0000

    Set up app for tests

--- test/functional/diagnostics_controller_test.rb
+++ test/functional/diagnostics_controller_test.rb
@@ -11,39 +11,8 @@ class DiagnosticsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:diagnostics)
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create diagnostic" do
-    assert_difference('Diagnostic.count') do
-      post :create, diagnostic: { description: @diagnostic.description, name: @diagnostic.name }
-    end
-
-    assert_redirected_to diagnostic_path(assigns(:diagnostic))
-  end
-
   test "should show diagnostic" do
     get :show, id: @diagnostic
     assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @diagnostic
-    assert_response :success
-  end
-
-  test "should update diagnostic" do
-    put :update, id: @diagnostic, diagnostic: { description: @diagnostic.description, name: @diagnostic.name }
-    assert_redirected_to diagnostic_path(assigns(:diagnostic))
-  end
-
-  test "should destroy diagnostic" do
-    assert_difference('Diagnostic.count', -1) do
-      delete :destroy, id: @diagnostic
-    end
-
-    assert_redirected_to diagnostics_path
   end
 end
