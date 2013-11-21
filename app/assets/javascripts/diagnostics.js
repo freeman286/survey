45e4daf8bced799aac2551466b205da2d9cb7280
Joel Mayer <joelmayer@Andys-Mac-Pro.local>
2013-11-21 18:36:36 +0000

    Finished progress bars

--- app/assets/javascripts/diagnostics.js
+++ app/assets/javascripts/diagnostics.js
@@ -2,9 +2,4 @@ $(document).ready(function() {
 	return $('.section').hover(function(event) {
 		return $(this).toggleClass("hover");
 	});
-	
-	$('#show-content').click(function(event) {
-		event.preventDefault();
-		alert("works");
-	});
 });
--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,21 +1,4 @@
 $(document).ready(function() {
-	
-	$('.yesx').click(function(event) {
-		var btn = $(this)
-		id = btn.data('id')
-		$('.yes.'+id).addClass("btn-success");
-		$('.no.'+id).removeClass("btn-danger");
-	});
-	
-	$('.nox').click(function(event) {
-		var btn = $(this)
-		id = btn.data('id')
-		$('.yes.'+id).removeClass("btn-success");
-		$('.no.'+id).addClass("btn-danger");
-
-	});
-	$('.section').popover();
-  
 	return $('.section').hover(function(event) {
     return $(this).toggleClass("hover");
   });
