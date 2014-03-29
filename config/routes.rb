9496bfa93a142c521ed4d0aed74f87376433f6f0
Joel Mayer <joelmayer@Andys-Mac-Pro.local>
2014-03-29 11:22:07 +0000

    Fixed divise routes

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,12 @@ Survey::Application.routes.draw do
   
   resources :users, :only => [:index, :show]
   
+  as :user do
+    get '/register', to: 'devise/registrations#new', :as => :register
+    get '/login', to: 'devise/sessions#new', :as => :login
+    get '/logout', to: 'devise/sessions#destroy', :as => :logout
+  end
+  
 
   devise_for :users
 
