a5c11988289fa6cdac1017b93d464be5c904ad75
Joel Mayer <joelmayer@Andys-Mac-Pro.local>
2014-03-31 19:22:27 +0100

    Made possible for sub_questions to have a value of 0

--- app/models/sub_question.rb
+++ app/models/sub_question.rb
@@ -10,7 +10,7 @@ class SubQuestion < ActiveRecord::Base
   validates :value, presence: true
   
   validates :value, presence: true, :numericality => {
-    greater_than: 0, less_than_or_equal_to: 100
+    greater_than_or_equal_to: 0, less_than_or_equal_to: 100
   }
   
   def yes?(user)
