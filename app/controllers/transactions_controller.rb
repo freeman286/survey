830aecc9c17b3909cbcc106e36c7195c0184f740
Joel Mayer <molemayer@gmail.com>
2015-05-31 16:01:26 +0100

    Fix typo

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,7 +2,7 @@ class TransactionsController < ApplicationController
 
   def paid
     if request.user_agent == "PayPal IPN ( https://www.paypal.com/ipn )"
-      Transaction.find_by_security_hash(params[:transaction_hash]).update_attribute(:completed => true)
+      Transaction.find_by_security_hash(params[:transaction_hash]).update_attributes(:completed => true)
     end
   end
 
