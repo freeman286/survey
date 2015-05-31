341ca8f47f9ff1dbc65dea6762c769b1222065cc
Joel Mayer <molemayer@gmail.com>
2015-05-31 09:08:55 +0100

    Fix syntax error

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,7 +2,7 @@ class TransactionsController < ApplicationController
 
   def paid
     puts ">>>>>>>>>>>>>>>>>>>>>#{request.user_agent}"
-    puts ">>>>>>>>>>>>>>>>>>>>>#{request.user_agent == "PayPal IPN ( https://www.paypal.com/ipn )}"
+    puts ">>>>>>>>>>>>>>>>>>>>>#{request.user_agent == "PayPal IPN ( https://www.paypal.com/ipn )"}"
   end
 
 end
