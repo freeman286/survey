b01b2d605d3e617b361e543d46cc142a9ed687bc
Joel Mayer <molemayer@gmail.com>
2015-06-03 16:20:54 +0100

    Add Paypal suff

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,6 +1,7 @@
 class TransactionsController < ApplicationController
 
   def paid
+    puts request.env
     if request.user_agent == "PayPal IPN ( https://www.paypal.com/ipn )"
       Transaction.find_by_security_hash(params[:transaction_hash]).update_attributes(:completed => true)
     end
