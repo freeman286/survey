ddeb5614fd2afb9e4afbe44e4e9cf8126de8c8ff
Andy Mayer <andy@yoomee.com>
2015-09-06 20:02:21 +0100

    Add better logging to transaction controller

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,9 +1,17 @@
 class TransactionsController < ApplicationController
 
   def paid
+    @status = false
+    @hash = params[:transaction_hash]
     if request.user_agent == "PayPal IPN ( https://www.paypal.com/ipn )" && params[:mc_gross] == "10.00"
-      Transaction.find_by_security_hash(params[:transaction_hash]).update_attributes(:completed => true)
+      @status = Transaction.find_by_security_hash(params[:transaction_hash]).update_attributes(:completed => true)
     end
+    if @status
+      puts "HASH: #{@hash} COMPLETED"
+    else
+      puts "HASH: #{@hash} NOT COMPLETED"      
+    end
+    render :layout => false
   end
 
   def thankyou
